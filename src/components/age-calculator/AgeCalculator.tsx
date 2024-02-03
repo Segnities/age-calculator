ed5e1213e337fe9546ab77b779e2a497cbadcdae
unknown <dimab787898@gmail.com>
2024-02-03 15:39:47 +0200

    colors & fonts

---
+++ src/components/age-calculator/AgeCalculator.tsx
@@ -0,0 +1,21 @@
+import './assets/scss/ageCalculator.scss';
+
+export default function AgeCalculator() {
+   return (
+      <div className="">
+         <div className="">
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+         </div>
+         <div>
+            <hr />
+         </div>
+         <div>
+            <div></div>
+            <div></div>
+            <div></div>
+         </div>
+      </div>
+   );
+}
