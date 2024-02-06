02e94af960a709c5f60b9cb573dbe2e177663c40
unknown <dimab787898@gmail.com>
2024-02-06 08:50:44 +0200

    inputs style

--- src/components/age-calculator/AgeCalculator.tsx
+++ src/components/age-calculator/AgeCalculator.tsx
@@ -4,9 +4,18 @@ export default function AgeCalculator() {
    return (
       <div className="age-container">
          <div className="age-inputs">
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+            <div className='inp-block'>
+               <label htmlFor="dd-input">Day</label>
+               <input type="text" id='dd-input' placeholder='DD' />
+            </div>
+            <div className='inp-block'>
+               <label htmlFor="mm-input">Month</label>
+               <input type="text" id='mm-input' placeholder='MM' />
+            </div>
+            <div className='inp-block'>
+               <label htmlFor="yyyy-input">Year</label>
+               <input type="text" id='yyyy-input' placeholder='YYYY' />
+            </div>
          </div>
          <div>
             <hr />
