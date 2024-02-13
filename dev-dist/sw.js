904fbddd456416685ead4063c051196c97e1435f
unknown <dimab787898@gmail.com>
2024-02-13 19:05:54 +0200

    pwa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1fkk1cpin48"
+    "revision": "0.77e3ldqu9h"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
