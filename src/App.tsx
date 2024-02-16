fde88ae25737797f6a42e726470c44cb9b7601e5
unknown <dimab787898@gmail.com>
2024-02-16 19:10:17 +0200

    push notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import DateDiffCalculator from "./components/dateDiffCalculator/DateDiffCalculator";
 import './App.scss';
+import { getToken } from "firebase/messaging";
+import { messaging } from "./firebase";
 
 function App() {
+  getToken(messaging, { vapidKey: import.meta.env.VITE_VAPID_KEY })
+    .then((currentToken) => {
+      if (currentToken) {
+        console.log(currentToken);
+      } else {
+        console.log('No registration token available. Request permission to generate one.');
+      }
+    }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+    });
   return (
     <div className="container">
-      <DateDiffCalculator/>
+      <DateDiffCalculator />
     </div>
   );
 }
